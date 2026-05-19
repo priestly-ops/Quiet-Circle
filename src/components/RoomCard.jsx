89542afe9086cff797f167bc2f3d5a14b059035d
priestly <priestlyraj@gmail.com>
2026-05-19 03:21:03 -0600

    Extract RoomCard component

---
+++ src/components/RoomCard.jsx
@@ -0,0 +1,18 @@
+export default function RoomCard({ room, selected, onSelect }) {
+  return (
+    <button
+      className={selected ? 'card roomCard selectedRoom' : 'card roomCard'}
+      onClick={() => onSelect(room)}
+    >
+      <div className="roomIcon">{room.icon}</div>
+
+      <div className="roomTop">
+        <h3>{room.name}</h3>
+        <span>{room.members}/8</span>
+      </div>
+
+      <p>{room.desc}</p>
+      <small>{room.theme}</small>
+    </button>
+  );
+}
