d798186a00ffd2f736e636b253d5a2876114ee63
priestly <priestlyraj@gmail.com>
2026-05-20 01:34:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { ReactionBar } from './components/EngagementCards';
 import Sidebar from './components/Sidebar';
 import RoomCard from './components/RoomCard';
 import LandingPage from './pages/LandingPage';
+import { ReactionBar } from './components/EngagementCards';
 import {
   DashboardEngagement,
   MoodAuraPreview,
